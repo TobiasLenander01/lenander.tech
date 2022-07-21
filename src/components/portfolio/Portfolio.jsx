f5f5bb7b23957ad2d8516d05c38f762319a62f20
Tobias Lenander <tobias.lenander@outlook.com>
2022-07-21 18:44:29 +0200

    Add portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.png'
+import IMG2 from '../../assets/portfolio2.png'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt="Reverse Recordings Game" />
+          </div>
+          <h3>Reverse Recordings Game</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/TobiasLenander01/Reverse-Recordings-Game" className='btn btn-primary' target='_blank'>Github</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG2} alt="Console Temperature Converter" />
+          </div>
+          <h3>Console Temperature Converter</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/TobiasLenander01/Temperature-Converter" className='btn btn-primary' target='_blank'>Github</a>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
