afd74d2d63ca839e1fab7fb694303b8288d25faf
Tobias Lenander <tobias.lenander@outlook.com>
2022-07-21 20:52:48 +0200

    Fix footer links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,10 +10,10 @@ const Footer = () => {
 
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
-        <li><a href="#About">About</a></li>
-        <li><a href="#Experience">Experience</a></li>
-        <li><a href="#Portfolio">Portfolio</a></li>
-        <li><a href="#Contact">Contact</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
 
       <div className="footer__socials">
