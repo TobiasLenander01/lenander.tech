9304b92ded99f491d6103baddc1ecbc081a7920a
Tobias Lenander <tobias.lenander@outlook.com>
2022-07-21 20:26:29 +0200

    Change Nav services to portfolio

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,7 +3,7 @@ import './nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
 import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
-import {MdOutlineDesignServices} from 'react-icons/md'
+import {BsBriefcase} from 'react-icons/bs'
 import {BiMessageSquareDetail} from 'react-icons/bi'
 import {useState} from 'react'
 
@@ -14,7 +14,7 @@ const Nav = () => {
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
-      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><MdOutlineDesignServices /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><BsBriefcase /></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
     </nav>
   )
