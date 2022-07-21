b50a85cd22c0c1052e82250a0a5b88080cddeed6
Tobias Lenander <tobias.lenander@outlook.com>
2022-07-21 19:54:17 +0200

    Implemented emailjs functionality

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_a5d2skj', 'template_qqpv3ms', form.current, 'itew9yYktx4FGcGrf')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset();
+  };
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -25,7 +41,7 @@ const Contact = () => {
             <a href="https://m.me/profile.php?id=100006995024917" target="_blank">Send a message</a>
           </article>
         </div>
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name='email' placeholder='Your Email' required />
           <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
